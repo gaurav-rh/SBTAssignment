2287951a25140c17bd8bed11fceee92f325f33d8
gaurav-rh <gaurav.raj@knoldus.com>
2021-11-26 16:29:35 +0530

    scala objects added /2n/ 2n

---
+++ common/src/main/scala/CommonFirst.scala
@@ -0,0 +1,6 @@
+object CommonFirst {
+
+    def main(args: Array[String]): Unit = {
+      println("Welcome to SBT First Assignment from module common By emp id 1722-Gaurav")
+    }
+  }
---
+++ rest/src/main/scala/RestFirst.scala
@@ -0,0 +1,5 @@
+object RestFirst {
+  def main(args: Array[String]): Unit = {
+      println("Welcome to SBT First Assignment from Rest module By emp id 1722-Gaurav")
+    }
+  }
